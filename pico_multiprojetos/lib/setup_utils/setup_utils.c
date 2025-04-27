f016864c231f9b0ffb91b6b6499a0393c81c5930
mister-rafael <93235866+mister-rafael@users.noreply.github.com>
2025-04-27 18:14:57 -0300

    Criação da biblioteca setup_utils
    Criação dos arquivos:
    CMakeLists, setup_utils.c e setup_utils.h.

---
+++ pico_multiprojetos/lib/setup_utils/setup_utils.c
@@ -0,0 +1,29 @@
+#include "setup_utils.h"
+#include "pico/stdlib.h" // Biblioteca padrão do Raspberry Pi Pico
+
+// Função para configurar o botão
+// Esta função inicializa o pino do botão como entrada e ativa o resistor de pull-up interno
+void iniciar_botao(uint pin) {
+    gpio_init(pin); // Inicializa o pino
+    gpio_set_dir(pin, GPIO_IN); // Define o pino como entrada
+    gpio_pull_up(pin); // Ativa o resistor de pull-up interno
+}
+
+// Função para iniciar o LED
+// Esta função inicializa o pino do LED como saída
+void iniciar_led(uint pin) {
+    gpio_init(pin); // Inicializa o pino
+    gpio_set_dir(pin, GPIO_OUT); // Define o pino como saída
+}
+
+// Função para iniciar a interface I2C
+// Esta função configura os pinos SDA e SCL, e define a taxa de transmissão
+// de dados para a interface I2C
+void iniciar_i2c(i2c_inst_t *i2c, uint sda_pin, uint scl_pin, uint baudrate_khz) {
+    i2c_init(i2c, baudrate_khz * 1000); // Inicializa a interface I2C com a taxa de transmissão especificada
+    gpio_set_function(sda_pin, GPIO_FUNC_I2C); // Configura o pino SDA para função I2C
+    gpio_set_function(scl_pin, GPIO_FUNC_I2C); // Configura o pino SCL para função I2C
+    gpio_pull_up(sda_pin); // Ativa o resistor de pull-up no pino SDA
+    gpio_pull_up(scl_pin); // Ativa o resistor de pull-up no pino SCL
+    
+}
---
+++ pico_multiprojetos/lib/setup_utils/setup_utils.h
@@ -0,0 +1,25 @@
+#ifndef SETUP_UTILS_H
+#define SETUP_UTILS_H
+
+// Include necessary headers
+#include <stdio.h>
+#include "hardware/i2c.h"
+
+// Definições de pinagem
+#define PIN_SDA 14 // Pino de dados do I2C
+#define PIN_SCL 15 // Pino de clock do I2C
+
+#define BUTTON_A_PIN 5 // Botão A
+#define BUTTON_B_PIN 6 // Botão B
+
+#define BLUE_LED_PIN 12  // LED azul
+#define RED_LED_PIN 13   // LED vermelho
+#define GREEN_LED_PIN 11 // LED verde
+
+// Declaração de funções para configuração do sistema e periféricos
+// -----------------------------------------------------------------------------------
+void iniciar_botao(uint pin); // Inicializa o botão na porta especificada
+void iniciar_led(uint pin); // Inicializa o LED na porta especificada
+void iniciar_i2c(i2c_inst_t *i2c, uint sda_pin, uint scl_pin, uint baudrate_khz); // Inicializa a interface I2C
+
+#endif // SETUP_UTILS_H
